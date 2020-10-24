797db0958e5afeb9b5ba61cd16be7ddb837c9a71
Vasiliy Korostelev <vasiliy.korostelev@gmail.com>
2020-10-25 00:24:02 +0300

    Improve getSearchItem method stability

--- ui/tests/_support/Page/Acceptance/VideoSearchPage.php
+++ ui/tests/_support/Page/Acceptance/VideoSearchPage.php
@@ -1,6 +1,7 @@
 <?php
 namespace Page\Acceptance;
 
+use Facebook\WebDriver\Exception\NoSuchElementException;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverElement;
 use PHPUnit\Framework\Assert;
@@ -12,26 +13,45 @@ class VideoSearchPage extends VideoPage
     public static string $searchItem = '.serp-item';
     public static string $searchItemThumbImage = '.serp-item .thumb-image__image';
     public static string $searchItemThumbVideo = '.serp-item .thumb-preview__target_playing .thumb-preview__video';
-    public static string $moreVideosButton = 'button.more__button';
+    public static string $moreVideosButton = '.more_last_yes button.more__button';
+    public static string $moreHiddenButton = '.more_disabled_yes .more__button_hidden_yes';
 
     public function seeSearchItems()
     {
         $this->acceptanceTester->seeNumberOfElements($this::$searchItem, [1, 20]);
     }
 
+    private function getSearchItems(): array
+    {
+        return $this->acceptanceTester->findElements($this::$searchItem);
+    }
+
     private function getSearchItem(int $itemNumber): WebDriverElement
     {
         $I = $this->acceptanceTester;
 
-        $items = $I->findElements($this::$searchItem);
+        $items = $this->getSearchItems();
+        $itemsCount = count($items);
 
-        while (count($items) < $itemNumber) {
-            $I->scrollToElement(end($items));
-            $items = $I->findElements($this::$searchItem);
-
-            if ($I->tryToSee('Ещё видео', $this::$moreVideosButton)) {
+        while ($itemsCount < $itemNumber) {
+            // Infinite scroll
+            if ($I->isElementPresent($this::$moreHiddenButton, 2)) {
+                $I->scrollToElement(end($items));
+            }
+            // Pagination button
+            elseif ($I->isElementPresent($this::$moreVideosButton, 2)) {
                 $I->Click($this::$moreVideosButton);
             }
+            // End of the list
+            else {
+                throw new NoSuchElementException("Can't find the ${itemNumber}'th element in search Results.");
+            }
+
+            $items = $I->waitFor(function() use ($itemsCount) {
+                $items = $this->getSearchItems();
+                return count($items) > $itemsCount ? $items : null;
+            }, 5);
+            $itemsCount = count($items);
         }
 
         return $items[$itemNumber];
